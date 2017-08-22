56106c2c7a019f950a75666dcbcd6ea27e401cc3
logonrm <logonrm@fiap.com.br>
2017-08-21 22:40:08 -0300

    Add project files.

---
+++ src/Fiap.PlataformaNet.Exercicio06.AppCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiap.PlataformaNet.Exercicio06.AppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
