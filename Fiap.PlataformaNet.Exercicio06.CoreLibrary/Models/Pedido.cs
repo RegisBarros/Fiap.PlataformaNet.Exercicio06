03889e53d70713fdedb9abdab63f11b071d96065
logonrm <logonrm@L2201MICRO18>
2017-08-21 22:48:38 -0300

    classes

---
+++ Fiap.PlataformaNet.Exercicio06.CoreLibrary/Models/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fiap.PlataformaNet.Exercicio06.CoreLibrary.Models
+{
+    class Pedido
+    {
+        public int PedidoId { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime Data { get; set; }
+    }
+}
---
+++ Fiap.PlataformaNet.Exercicio06.CoreLibrary/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fiap.PlataformaNet.Exercicio06.CoreLibrary.Models
+{
+    class Produto
+    {
+    }
+}
